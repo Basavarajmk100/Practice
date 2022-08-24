2502617343ef6cb1af7fa8fce1c91c436e85bc50
Admin <Admin@DESKTOP-GADFEBD>
2022-08-24 15:26:56 +0530

    Updated

--- sdet38_practice_github/src/test/java/com/practice_github/Github.java
+++ sdet38_practice_github/src/test/java/com/practice_github/Github.java
@@ -2,7 +2,8 @@ package com.practice_github;
 
 public class Github {
 	public static void main() {
-		System.out.println("Sample");
+		System.out.println("Sample1");
+		System.out.println("Sample2");
 		
 	}
 	
---
+++ sdet38_practice_github/src/test/java/com/practice_github/Github2.java
@@ -0,0 +1,10 @@
+package com.practice_github;
+
+public class Github2 {
+	public static void main() {
+		System.out.println("github sample 2");
+	
+
+}
+	
+}
