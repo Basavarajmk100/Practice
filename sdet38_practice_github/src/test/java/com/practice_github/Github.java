c886e3a56002198abe46b9d3e4fd96df86f9502e
Basavarajmk100 <111878215+Basavarajmk100@users.noreply.github.com>
2022-08-24 15:45:47 +0530

    Revert "Updated"

--- sdet38_practice_github/src/test/java/com/practice_github/Github.java
+++ sdet38_practice_github/src/test/java/com/practice_github/Github.java
@@ -2,8 +2,7 @@ package com.practice_github;
 
 public class Github {
 	public static void main() {
-		System.out.println("Sample1");
-		System.out.println("Sample2");
+		System.out.println("Sample");
 		
 	}
 	
