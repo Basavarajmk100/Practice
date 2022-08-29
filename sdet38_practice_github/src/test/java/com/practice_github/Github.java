b7c6dc6a9b7571b2a35dbd87016fc0a6e863a9c3
Admin <Admin@DESKTOP-GADFEBD>
2022-08-29 13:05:31 +0530

    test

--- sdet38_practice_github/src/test/java/com/practice_github/Github.java
+++ sdet38_practice_github/src/test/java/com/practice_github/Github.java
@@ -2,8 +2,8 @@ package com.practice_github;
 
 public class Github {
 	public static void main() {
-		System.out.println("Sample1");
-		System.out.println("Sample2");
+		System.out.println("This is gb1");
+	
 		
 	}
 	
